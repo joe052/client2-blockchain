2355eae30529e80a88add4017b2c83b24c25f2a4
Joe Karanja <87515353+joe052@users.noreply.github.com>
2022-05-02 12:57:09 +0000

    set up the files

--- index.js
+++ index.js
@@ -9,5 +9,5 @@
 //const bb1 = require('./newProject/front.js');
 //const bb1 = require('./newProject/complex.js');
 //const bb2 = require('./old/server.js');
-//const bb2 = require('./restructure/server.js');
-console.log("brooo!");
+const bb2 = require('./restructure/server.js');
+//console.log("brooo!");
