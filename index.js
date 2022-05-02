4cbee87e8d5a00ca8423a12567cc065c87e92351
Joe Karanja <87515353+joe052@users.noreply.github.com>
2022-05-02 12:30:42 +0000

    updated packages

--- index.js
+++ index.js
@@ -9,4 +9,5 @@
 //const bb1 = require('./newProject/front.js');
 //const bb1 = require('./newProject/complex.js');
 //const bb2 = require('./old/server.js');
-const bb2 = require('./restructure/server.js');
+//const bb2 = require('./restructure/server.js');
+console.log("brooo!");
